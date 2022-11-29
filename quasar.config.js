2c0c00eb1d7c4c69237f773342c19f04bea3311d
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2022-11-29 15:27:26 +0000

    Changed router to history mode

--- quasar.config.js
+++ quasar.config.js
@@ -60,14 +60,14 @@ module.exports = configure(function (/* ctx */) {
         node: 'node16'
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: 'alvasori',
+      // publicPath: 'alvasori',
       // analyze: true,
       env: require('dotenv').config().parsed,
       // rawDefine: {}
