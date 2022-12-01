53efaa9782588194584c94845e5b950d32fb5630
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2022-12-01 07:39:07 +0000

    Fixed gitpod initialization

--- src/composables/firebase/use-firebase.ts
+++ src/composables/firebase/use-firebase.ts
@@ -3,15 +3,7 @@ import { initializeApp, getApps, FirebaseOptions } from 'firebase/app'
 export * from 'firebase/app'
 
 export function getEnvConfig (): FirebaseOptions {
-  return {
-    apiKey: process.env.FB_API_KEY,
-    authDomain: process.env.FB_AUTH_DOMAIN,
-    projectId: process.env.FB_PROJECT_ID,
-    storageBucket: process.env.FB_STORAGE_BUCKET,
-    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-    appId: process.env.FB_APP_ID,
-    measurementId: process.env.FB_MEASUREMENT_ID
-  }
+  return JSON.parse(process.env.FIREBASE_CONFIG ?? '{}') as FirebaseOptions
 }
 
 export function initializeFirebase (config?: FirebaseOptions) {
