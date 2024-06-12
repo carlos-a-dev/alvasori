2b3bf09b0c8e96a5bef9162ede4cba595d02cd8b
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2024-06-11 21:44:51 -0400

    Change package manager to pnpm.

--- .server/stage/build.sh
+++ .server/stage/build.sh
@@ -1,11 +1,11 @@
 # Pull and deploy
 
-yarn
+pnpm i
 
 export DATABASE_URL="file:../db/alvasori.db"
 npx prisma generate
 npx prisma migrate deploy
 unset DATABASE_URL
 
-yarn build
+pnpm build
 pm2 --update-env restart alvasori-stage
